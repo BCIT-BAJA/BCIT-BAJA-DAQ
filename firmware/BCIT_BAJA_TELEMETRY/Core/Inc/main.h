6bb2db35d95fde02befa5f36ef84693d06aaa830
Parry <parryzhuo@gmail.com>
2025-07-27 20:09:02 -0700

    re-re-re-re-re routed PCB

--- firmware/BCIT_BAJA_TELEMETRY/Core/Inc/main.h
+++ firmware/BCIT_BAJA_TELEMETRY/Core/Inc/main.h
@@ -63,6 +63,10 @@ void Error_Handler(void);
 #define USART_TX_GPIO_Port GPIOA
 #define USART_RX_Pin GPIO_PIN_3
 #define USART_RX_GPIO_Port GPIOA
+#define strainEN_Pin GPIO_PIN_6
+#define strainEN_GPIO_Port GPIOC
+#define V_batt_en_Pin GPIO_PIN_9
+#define V_batt_en_GPIO_Port GPIOC
 #define TMS_Pin GPIO_PIN_13
 #define TMS_GPIO_Port GPIOA
 #define TCK_Pin GPIO_PIN_14
